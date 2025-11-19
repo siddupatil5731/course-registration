0502ab6ffaf6ac4eb6377dc74ee9461fb5077662
siddu <patilsiddu267@gmail.com>
2025-11-19 10:15:59 +0530

    3rd commit

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
+const path = require("path");
+
+// Serve static frontend
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
